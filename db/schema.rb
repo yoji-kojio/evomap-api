4691cbffe98ac1f98962f3460f606eacc5cce901
yoji-kojio <yoji.kojio@gmail.com>
2019-11-29 22:18:26 -0300

    Create Requirement model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_011318) do
+ActiveRecord::Schema.define(version: 2019_11_30_011810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "careers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "requirements", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
