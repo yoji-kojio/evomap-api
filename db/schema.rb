feff497200018e24c7be0b240d274e6b5fc8a4c5
yoji-kojio <yoji.kojio@gmail.com>
2019-12-01 12:40:31 -0300

    Add username column to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_043255) do
+ActiveRecord::Schema.define(version: 2019_12_01_153706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_043255) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "career_requirements", "careers"
